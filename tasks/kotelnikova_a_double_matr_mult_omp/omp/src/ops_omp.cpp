0c18491ec49f0b408784ea2755215f6e378c39ed
KotelnikovaA <rabatenok@gmail.com>
2026-02-25 22:27:20 +0300

    second ver 1

--- tasks/kotelnikova_a_double_matr_mult_omp/omp/src/ops_omp.cpp
+++ tasks/kotelnikova_a_double_matr_mult_omp/omp/src/ops_omp.cpp
@@ -73,60 +73,60 @@ bool KotelnikovaATaskOMP::PreProcessingImpl() {
 
 SparseMatrixCCS KotelnikovaATaskOMP::MultiplyMatrices(const SparseMatrixCCS &a, const SparseMatrixCCS &b) {
   SparseMatrixCCS result(a.rows, b.cols);
-  
+
   const double epsilon = 1e-10;
-  
+
   std::vector<int> col_start(b.cols + 1, 0);
   std::vector<int> col_end(b.cols + 1, 0);
-  
-  #pragma omp parallel for schedule(dynamic, 8)
+
+#pragma omp parallel for schedule(dynamic, 8)
   for (int j = 0; j < b.cols; ++j) {
     std::vector<double> temp(a.rows, 0.0);
-    
+
     for (int b_idx = b.col_ptrs[j]; b_idx < b.col_ptrs[j + 1]; ++b_idx) {
       const int k = b.row_indices[b_idx];
       const double b_val = b.values[b_idx];
-      
+
       for (int a_idx = a.col_ptrs[k]; a_idx < a.col_ptrs[k + 1]; ++a_idx) {
         const int i = a.row_indices[a_idx];
         temp[i] += a.values[a_idx] * b_val;
       }
     }
-    
+
     int nnz_in_col = 0;
     for (int i = 0; i < a.rows; ++i) {
       if (std::abs(temp[i]) > epsilon) {
         nnz_in_col++;
       }
     }
-    
+
     col_start[j] = nnz_in_col;
   }
-  
+
   std::vector<int> col_ptr(b.cols + 1, 0);
   for (int j = 0; j < b.cols; ++j) {
     col_ptr[j + 1] = col_ptr[j] + col_start[j];
   }
-  
+
   int total_nnz = col_ptr[b.cols];
   result.values.resize(total_nnz);
   result.row_indices.resize(total_nnz);
   result.col_ptrs = col_ptr;
-  
-  #pragma omp parallel for schedule(dynamic, 8)
+
+#pragma omp parallel for schedule(dynamic, 8)
   for (int j = 0; j < b.cols; ++j) {
     std::vector<double> temp(a.rows, 0.0);
-    
+
     for (int b_idx = b.col_ptrs[j]; b_idx < b.col_ptrs[j + 1]; ++b_idx) {
       const int k = b.row_indices[b_idx];
       const double b_val = b.values[b_idx];
-      
+
       for (int a_idx = a.col_ptrs[k]; a_idx < a.col_ptrs[k + 1]; ++a_idx) {
         const int i = a.row_indices[a_idx];
         temp[i] += a.values[a_idx] * b_val;
       }
     }
-    
+
     int pos = col_ptr[j];
     for (int i = 0; i < a.rows; ++i) {
       if (std::abs(temp[i]) > epsilon) {
@@ -136,7 +136,7 @@ SparseMatrixCCS KotelnikovaATaskOMP::MultiplyMatrices(const SparseMatrixCCS &a, 
       }
     }
   }
-  
+
   return result;
 }
 
